9ff885b257910a18c8e86a7ded5ef5bf0cdc069b
brendanf <brendan.furneaux@gmail.com>
2021-02-12 14:28:02 +0100

    use only eukaryotic silva, add comments

--- scripts/05_format_silva.R
+++ scripts/05_format_silva.R
@@ -5,7 +5,7 @@
 
 # load just the fasta headers
 ref <- Biostrings::readDNAStringSet(
-    here::here("reference", "SILVA_138.1_LSURef_NR99_tax_silva_dna.fasta.gz")
+    here::here("reference", "SILVA_138.1_LSURef_NR99_tax_silva_dna_euk_trim.fasta.gz")
 )
 
 # parse the headers into accession number and taxonomy
@@ -19,7 +19,11 @@ headers <- tidyr::extract(
     regex = "(.+);([^;]+)$"
 )
 
-
+# This function taxes a character vector, and returns a vector of the same
+# length, cumulatively joining the elements of the vector, with ";" as a separator.
+# e.g., c("Fungi", "Basidiomycota", "Agaricomycetes", "Sebacinales") ->
+# c("Fungi", "Fungi;Basidiomycetes", "Fungi;Basidiomycetes;Agaricomycetes",
+#   "Fungi;Basidiomycetes;Agaricomycetes;Sebacinales")
 cumcollapse <- function(x) {
     purrr::map_chr(
         seq_along(x),
@@ -32,8 +36,10 @@ all_taxa <- dplyr::distinct(headers, taxonomy)
 
 # for each terminal taxon, make a list of all its supertaxa,
 # and fully qualify each of them
+# don't include "Incertae Sedis" taxa; those will be recreated later.
 all_taxa$taxon <- all_taxa$taxonomy
 all_taxa <- tidyr::separate_rows(all_taxa, taxon, sep = ";")
+all_taxa <- dplyr::filter(all_taxa, !endsWith(taxon, "Incertae Sedis"))
 all_taxa <- dplyr::group_by(all_taxa, taxonomy)
 all_taxa <- dplyr::mutate(all_taxa, full_taxon = cumcollapse(taxon))
 all_taxa <- dplyr::mutate(all_taxa, full_taxon = paste0(full_taxon, ";"))
@@ -47,17 +53,31 @@ taxa <- readr::read_delim(
 )
 
 # all the ranks included in the taxonomy, in order from highest to lowest
-silva_ranks <- c("domain", "major_clade", "superkingdom", "kingdom", "subkingdom", "superphylum", "phylum", "subphylum", "infraphylum", "superclass", "class", "subclass", "infraclass", "superorder", "order", "suborder", "superfamily", "family", "subfamily", "genus")
+silva_ranks <- c("domain", "major_clade", "superkingdom", "kingdom",
+                 "subkingdom", "superphylum", "phylum", "subphylum",
+                 "infraphylum", "superclass", "class", "subclass", "infraclass",
+                 "superorder", "order", "suborder", "superfamily", "family",
+                 "subfamily", "genus")
 
-all_taxa <- dplyr::left_join(all_taxa, taxa, by = c("full_taxon" ="taxonomy"))
+# add rank information to all the taxa
+all_taxa <- dplyr::left_join(all_taxa, taxa, by = c("full_taxon" = "taxonomy"))
+all_taxa <- dplyr::mutate(
+    all_taxa,
+    rank = factor(rank, levels = silva_ranks, ordered = TRUE)
+)
 
-all_taxa <- dplyr::mutate(all_taxa, rank = factor(rank, levels = silva_ranks, ordered = TRUE))
-
+# make a table giving the ranked classification for each terminal taxon
 all_taxa <- dplyr::select(all_taxa, taxonomy, taxon, rank)
 all_taxa <- dplyr::group_by(all_taxa, taxonomy, rank)
 all_taxa <- dplyr::summarise(all_taxa, taxon = dplyr::first(taxon))
 all_taxa <- dplyr::ungroup(all_taxa)
 all_taxa <- tidyr::pivot_wider(all_taxa, names_from = "rank", values_from = "taxon")
+
+# make values for all the primary ranks using given values
+# when there is no given value, take the name from the next
+# higher rank where there is a value.
+# If no lower rank is given, use "unidentified_"
+# If there is a lower rank given, use "_Incertae_sedis"
 unknown = "unidentified_"
 incertae = "_Incertae_sedis"
 interpolate_rank <- function(x, x2, genus2) {
@@ -88,6 +108,8 @@ all_taxa <- dplyr::mutate(
     sintax = glue::glue("tax=d:{domain},k:{kingdom},p:{phylum},c:{class},o:{order},f:{family},g:{genus}")
 )
 
+# output in dada2 and sintax formats
+
 headers <- dplyr::left_join(
     headers,
     dplyr::select(all_taxa, taxonomy, dada2, sintax),
@@ -106,5 +128,7 @@ Biostrings::writeXStringSet(
     here::here("reference", "silva.LSU.sintax.fasta.gz"),
     compress = TRUE
 )
+
+# clean up
 remove(ref, ref_dada, ref_sintax, headers, taxa, all_taxa)
 gc()
