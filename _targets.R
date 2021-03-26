253048d0d236419938dc0882259bee12b4aba051
brendanf <brendan.furneaux@gmail.com>
2021-03-26 09:28:00 +0100

    rename/reorder R scripts

--- _targets.R
+++ _targets.R
@@ -2,11 +2,12 @@ library(targets)
 library(tarchetypes)
 
 # defines lsux_plan
-# source(here::here("scripts", "01_lsux.R"))
-# source(here::here("scripts", "02_dada2.R"))
-source(here::here("scripts", "03_otu_tables.R"))
-source(here::here("scripts", "04_compare_tree.R"))
-source(here::here("scripts", "06_taxonomy.R"))
+source(here::here("scripts", "01_functions.R"))
+# source(here::here("scripts", "02_lsux.R"))
+# source(here::here("scripts", "03_dada2.R"))
+source(here::here("scripts", "04_otu_tables.R"))
+source(here::here("scripts", "05_taxonomy.R"))
+source(here::here("scripts", "06_compare_tree.R"))
 list(
   otu_table_plan,
   compare_tree_targets,
